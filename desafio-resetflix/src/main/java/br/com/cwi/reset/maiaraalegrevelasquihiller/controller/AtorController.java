824e149bc4b5c916852e6f2003508344f11738ee
Maiara Hiller <maiaravelasqui@gmail.com>
2021-10-20 17:56:40 -0300

    Diretor Controller post e get

--- desafio-resetflix/src/main/java/br/com/cwi/reset/maiaraalegrevelasquihiller/controller/AtorController.java
+++ desafio-resetflix/src/main/java/br/com/cwi/reset/maiaraalegrevelasquihiller/controller/AtorController.java
@@ -1,12 +1,14 @@
 package br.com.cwi.reset.maiaraalegrevelasquihiller.controller;
 
 import br.com.cwi.reset.maiaraalegrevelasquihiller.FakeDatabase;
+import br.com.cwi.reset.maiaraalegrevelasquihiller.model.Ator;
 import br.com.cwi.reset.maiaraalegrevelasquihiller.request.AtorRequest;
 import br.com.cwi.reset.maiaraalegrevelasquihiller.response.AtorEmAtividade;
 import br.com.cwi.reset.maiaraalegrevelasquihiller.service.AtorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,7 +23,6 @@ public class AtorController {
         this.atorService = new AtorService(FakeDatabase.getInstance());
     }
 
-    //demais métodos
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void criarAtor(@RequestBody AtorRequest atorRequest) throws Exception {
@@ -32,6 +33,17 @@ public class AtorController {
     @ResponseStatus(HttpStatus.OK)
     public List<AtorEmAtividade> listarAtoresEmAtividade(@RequestParam String filtroNome) throws Exception {
         return this.atorService.listarAtoresEmAtividade(filtroNome);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Ator consultarAtor(@PathVariable Integer id) throws Exception{
+       return this.atorService.consultarAtor(id);
+    }
+
+    @GetMapping
+    public List<Ator> consultarAtores() throws Exception{
+        return this.atorService.consultarAtores();
     }
 }
 
---
+++ desafio-resetflix/src/main/java/br/com/cwi/reset/maiaraalegrevelasquihiller/controller/DiretorController.java
@@ -0,0 +1,39 @@
+package br.com.cwi.reset.maiaraalegrevelasquihiller.controller;
+
+import br.com.cwi.reset.maiaraalegrevelasquihiller.FakeDatabase;
+import br.com.cwi.reset.maiaraalegrevelasquihiller.model.Diretor;
+import br.com.cwi.reset.maiaraalegrevelasquihiller.request.DiretorRequest;
+import br.com.cwi.reset.maiaraalegrevelasquihiller.service.DiretorService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/diretores")
+public class DiretorController {
+    private DiretorService diretorService;
+
+    public DiretorController() {this.diretorService = new DiretorService(FakeDatabase.getInstance());}
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void cadastrarDiretor(@RequestBody DiretorRequest diretorRequest) throws Exception{
+        this.diretorService.cadastrarDiretor(diretorRequest);
+    }
+
+    @GetMapping("/diretores")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Diretor> listarDiretores(@RequestParam String filtroNome)throws Exception{
+        return this.diretorService.listarDiretores(filtroNome);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Diretor consultarDiretor(@PathVariable Integer id)throws Exception{
+        return this.diretorService.consultarDiretor(id);
+    }
+
+
+}
